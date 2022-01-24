1af524cd9823edfc667dc3201a7e77004dfee0de
Italo <italosanttana@hotmail.com>
2022-01-24 04:40:49 -0300

    feat(init): ༼ つ ◕_◕ ༽つworking-env

---
+++ index-express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", function(req, res){
+	res.send("Olá, mundinho do GET");
+});
+
+app.listen(port, ()=> {
+	console.info("Aplicação rodando em http://localhost:3000");
+});
---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+
+const variavelTest = process.env.TESTE;
+
+console.log("Olá", variavelTest);
